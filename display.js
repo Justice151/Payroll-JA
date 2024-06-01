e092154e586bde793d5c62932eff156589260ca6
Justice151 <75762021+Justice151@users.noreply.github.com>
2024-06-01 10:35:50 -0700

    Update display.js

--- display.js
+++ display.js
@@ -12,6 +12,7 @@ function displayEntries() {
         row.insertCell(1).innerText = entry.hoursWorked;
         row.insertCell(2).innerText = entry.workDescription;
         row.insertCell(3).innerText = entry.hourlyRate;
-        row.insertCell(4).innerText = entry.totalPayment.toFixed(2);
+        row.insertCell(4).innerText = entry.workDate;
+        row.insertCell(5).innerText = entry.totalPayment.toFixed(2);
     });
 }
